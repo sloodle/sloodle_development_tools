e32d7b5132489ffbf059fa8f8ed352f3faaa88ab
Edmund Edgar <ed@socialminds.jp>
2011-10-21 02:43:28 +0000

    Rewrote the tarball script to make the build itself rather than relying on submodules, which are confusing me.

--- deployment/make_tarballs.sh
+++ deployment/make_tarballs.sh
@@ -8,38 +8,84 @@ TAR=/bin/tar
 ZIP="/usr/bin/zip -q"
 SLOODLETOPDIR=moodle_wwwroot
 
-
-
 if [ "$1" = "" ]; then
-   echo "Usage: sloodle_development_tools/deployment/make_tarball.sh <tag> <location>"
+   echo "Usage: sloodle_development_tools/deployment/make_tarball.sh <tag> <working_dir> <location>"
    exit
 fi
 
 if [ "$2" = "" ]; then
-   echo "Usage: sloodle_development_tools/deployment/make_tarball.sh <tag> <location>"
+   echo "Usage: sloodle_development_tools/deployment/make_tarball.sh <tag> <working_dir> <location>"
    exit
 fi
 
+if [ "$3" = "" ]; then
+   echo "Usage: sloodle_development_tools/deployment/make_tarball.sh <tag> <working_dir> <location>"
+   exit
+fi
 
 echo $TAG;
 
-if [ ! -d sloodle_development_tools ]; then
-   echo "The tag_release script must be run from inside the sloodle_all_submodules directory."
+TAG=$1
+WORKINGDIR=$2
+SAVEDIR=$3
+
+if [ -d "$WORKINGDIR" ]; then
+   echo "Error: Working directory $WORKINGDIR already exists, exiting."
    exit
+else
+   mkdir "$WORKINGDIR"
 fi
 
-TAG=$1
-SAVEDIR=$2
-echo "Updating to version $TAG"
-git checkout $TAG
-
-echo "Updating submodules"
-git submodule init
-git submodule update
-
 echo "Creating tarball and zip for tag $TAG"
+mkdir "$WORKINGDIR/$SLOODLETOPDIR"
+cd "$WORKINGDIR/$SLOODLETOPDIR"
 
+# Everything goes under moodle_wwwroot
+
+# The module goes under mod/sloodle
+mkdir mod
+cd mod
+wget -O "$TAG.tar.gz" "https://github.com/sloodle/moodle-mod_sloodle/tarball/$TAG"
+tar zxvf "$TAG.tar.gz"
+rm "$TAG.tar.gz"
+mv sloodle-moodle-* sloodle
+cd ..
+
+# The two blocks go under blocks/
+
+
+
+mkdir blocks
+cd blocks
+
+wget -O "$TAG.tar.gz" "https://github.com/sloodle/moodle-block_sloodle_menu/tarball/$TAG"
+tar zxvf "$TAG.tar.gz"
+rm "TAG.tar.gz"
+mv sloodle-moodle-* sloodle_menu
+
+wget -O "$TAG.tar.gz" "https://github.com/sloodle/moodle-block_sloodle_backpack/tarball/$TAG"
+tar zxvf "$TAG.tar.gz"
+rm "$TAG.tar.gz"
+mv sloodle-moodle-* sloodle_backpack
+
+cd ..
+
+# The assignment plugin goes under mod/assignment/type
+
+mkdir mod/assignment
+mkdir mod/assignment/type
+cd mod/assignment/type
+
+wget -O "$TAG.tar.gz" "https://github.com/sloodle/moodle-assignment_sloodleobject/tarball/$TAG"
+tar zxvf "$TAG.tar.gz"
+rm "$TAG.tar.gz"
+mv sloodle-moodle-* sloodleobject
+
+cd ../../..
+
+cd ..
 ${TAR} zcf "${SAVEDIR}/sloodle_${TAG}.tar.gz" ${SLOODLETOPDIR}/
 ${ZIP} "${SAVEDIR}/sloodle_${TAG}.zip" -r ${SLOODLETOPDIR}/
 
-
+cd ..
+rm -rf "${WORKINGDIR}"
