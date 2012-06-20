3886afddfb2bf2a92fff9f5e831b565390fafd24
Edmund Edgar <ed@socialminds.jp>
2012-06-20 00:36:38 +0000

    Fixed to work with command-line arguments

--- backup_restore/backuptest.php
+++ backup_restore/backuptest.php
@@ -1,20 +1,42 @@
 <?php
-// Backup test script
-// Edmund Edgar, 2012-06
-
-// Used in the avatar classroom environment - will normally be ignored
 $_SERVER = array();
 $_SERVER['SERVER_NAME'] = 'gershwinklata1.avatarclassroom.com';
  
 define('CLI_SCRIPT', 1);
 require_once('config.php');
 require_once($CFG->dirroot . '/backup/util/includes/backup_includes.php');
- 
+
+$backup_types = array(
+    'course' => backup::TYPE_1COURSE,
+    'activity' => backup::TYPE_1ACTIVITY
+);
+
+if (count($argv) < 3) {
+    print "Usage: ".$argv[0]." <user> <type> <course_or_activity_id>\n";
+    exit;
+}
+
+$user_doing_the_backup   = intval($argv[1]); //2; // Set this to the id of your admin accouun
+
+$backup_type_arg = $argv[2];
+
+if (!isset($backup_types[$backup_type_arg])) {
+    print "Backup type $backup_type_arg not supported.\n";
+    exit;
+}
+
+$backup_type = $backup_types[$backup_type_arg];
+
 //$course_module_to_backup = 2; // Set this to one existing choice cmid in your dev site
-$course_module_to_backup = 3; // Set this to one existing choice cmid in your dev site
-$user_doing_the_backup   = 2; // Set this to the id of your admin accouun
+$course_module_to_backup = intval($argv[3]); //27; //3; // Set this to one existing choice cmid in your dev site
+
+if (!$course_module_to_backup || !$user_doing_the_backup) {
+    print "Parameters wrong\n";
+    exit;
+}
+
  
-$bc = new backup_controller(backup::TYPE_1ACTIVITY, $course_module_to_backup, backup::FORMAT_MOODLE, backup::INTERACTIVE_NO, backup::MODE_GENERAL, $user_doing_the_backup);
+$bc = new backup_controller($backup_type, $course_module_to_backup, backup::FORMAT_MOODLE, backup::INTERACTIVE_NO, backup::MODE_GENERAL, $user_doing_the_backup);
 //$bc = new backup_controller(backup::TYPE_1COURSE, $course_module_to_backup, backup::FORMAT_MOODLE, backup::INTERACTIVE_NO, backup::MODE_GENERAL, $user_doing_the_backup);
 
 $bc->execute_plan();
--- backup_restore/restoretest.php
+++ backup_restore/restoretest.php
@@ -7,34 +7,66 @@ require_once('config.php');
 require_once($CFG->dirroot . '/backup/util/includes/backup_includes.php');
 require_once($CFG->dirroot . '/backup/util/includes/restore_includes.php');
 
+$backup_types = array(
+    'course' => backup::TYPE_1COURSE,
+    'activity' => backup::TYPE_1ACTIVITY
+);
 
-/*
-Find the latest backup created with backuptest.php
-}/
+if (count($argv) < 2) {
+    print "Usage: ".$argv[0]." <user> <type> <course> [<backup_folder>]\n";
+    exit;
+}
+
+$user_doing_the_backup   = intval($argv[1]); //2; // Set this to the id of your admin accouun
+
+$backup_type_arg = $argv[2];
+
+if (!isset($backup_types[$backup_type_arg])) {
+    print "Backup type $backup_type_arg not supported.\n";
+    exit;
+}
+
+//$course_module_to_backup = 2; // Set this to one existing choice cmid in your dev site
+$courseid = intval($argv[3]); //27; //3; // Set this to one existing choice cmid in your dev site
+
+if (!$user_doing_the_backup) {
+    print "Parameters wrong\n";
+    exit;
+}
+
+if (!$courseid && ($backup_type == backup::TYPE_1ACTIVITY) ) {
+    print "For a backup of type $backup_type_arg, you need to specify a course ID to restore to.\n";
+    exit;
+}
 
 $backupdir = '../resources/moodledata/temp/backup';
 
-$folder = null;
-$highestmtime = 0;
-foreach (glob($backupdir.'/*') as $f) {
-    if (!is_dir($f)) {
-        continue;
-    }
-    if ( !$folder || (filemtime($f) > $highestmtime) ) {
-        $folder = $f;
-        $highestmtime = filemtime($f);
-    }
-}   
+if (isset($argv[4])) {
 
-$folderbits = explode('/',$folder); 
-$folder = array_pop($folderbits);
-echo "About to start restore from folder $folder\n";
+    $folder = $argv[4];
 
-$courseid = 3;
+} else {
+     
+    $folder = null;
+    $highestmtime = 0;
+    foreach (glob($backupdir.'/*') as $f) {
+        if (!is_dir($f)) {
+            continue;
+        }
+        if ( !$folder || (filemtime($f) > $highestmtime) ) {
+            $folder = $f;
+            $highestmtime = filemtime($f);
+        }
+    }   
 
+    $folderbits = explode('/',$folder); 
+    $folder = array_pop($folderbits);
 
-/*
- // Uncomment to create a course
+}
+
+echo "About to start restore from backup folder $folder\n";
+
+if (!$courseid) {
  // Get or create category.
     $categoryname = 'Restore test';
     $categoryid = $DB->get_field( 'course_categories', 'id', array('name'=>$categoryname) );
@@ -52,21 +84,18 @@ $courseid = 3;
 
     // Create new course.
     $courseid = restore_dbops::create_new_course( $fullname, $shortname, $categoryid );
-*/
 
-
-
+} 
 
 // Transaction
 $transaction = $DB->start_delegated_transaction();
 
 // Create new course
 //$courseid = restore_dbops::create_new_course($fullname, $shortname, $categoryid);
-$userid = 2;
 
 // Restore backup into course
 $controller = new restore_controller($folder, $courseid, 
-        backup::INTERACTIVE_NO, backup::MODE_SAMESITE, $userid,
+        backup::INTERACTIVE_NO, backup::MODE_SAMESITE, $user_doing_the_backup,
         backup::TARGET_NEW_COURSE);
 $controller->execute_precheck();
 $controller->execute_plan();
