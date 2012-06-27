824f1a372efd48747461de50a8996367ad6c5492
Edmund Edgar <online@edochan.com>
2012-06-27 08:33:56 +0000

    Scripts for renaming lots of LSL scripts in one go. NB renameLSL.sh is probably wrong and throws out lots of errors, but it worked.

---
+++ opensim_sync/renameLSL.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+
+BEFORE=$1
+AFTER=$2
+
+pushd ../../moodle-mod_sloodle
+if [ ! -f "$AFTER" ]; then
+	if [ ! -d "$AFTER" ]; then
+		mkdir -p $AFTER
+		rmdir $AFTER
+		git mv $BEFORE $AFTER
+	fi
+	git mv $BEFORE $AFTER
+fi
+popd
+
+pushd ../../avatarclassroom_opensim_iar/iar/assets
+perl -i -pe "s#$BEFORE#$AFTER#g" *.lsl
+popd
+
+pushd ../../sloodle_opensim_iar/iar/assets
+perl -i -pe "s#$BEFORE#$AFTER#g" *.lsl
+popd
