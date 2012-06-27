a65d85a85a18d1c3aa3f438059da0be037246315
Edmund Edgar <online@edochan.com>
2012-06-27 01:35:43 +0000

    Fixed comment to use the actual path I'll need

--- opensim_sync/syncOarScriptsToSVN.sh
+++ opensim_sync/syncOarScriptsToSVN.sh
@@ -12,7 +12,7 @@
 # Example of intended use:
 # cd working/sloodle_opensim_iar
 # git pull
-# find iar/ -name '*.lsl' -exec sloodle_development_tools/opensim_sync/syncSVNScriptsToOar.sh {} \;
+# find iar/ -name '*.lsl' -exec ../sloodle_development_tools/opensim_sync/syncSVNScriptsToOar.sh {} \;
 # git diff iar | more
 # # Scan through the changes to make sure they make sense
 # git add -a
